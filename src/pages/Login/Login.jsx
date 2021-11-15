783bb1f39d541d5a3f0b4dc151b3897105ea4be5
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2021-11-15 10:35:50 -0500

    Add links at login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Card, Button, FloatingLabel, Form } from 'react-bootstrap';
 
 import './Login.scss';
@@ -26,12 +27,18 @@ const Login = () => {
                         <FloatingLabel
                             controlId="floatingPassword"
                             label="Password"
+                            className="mb-3"
                         >
                             <Form.Control
                                 type="password"
                                 placeholder="Password"
                             />
                         </FloatingLabel>
+                        <Form.Check
+                            type="checkbox"
+                            id="checkbox"
+                            label="Remember Password"
+                        />
                         <Button
                             variant="primary"
                             className="d-block w-100 mt-3"
@@ -40,6 +47,9 @@ const Login = () => {
                         </Button>
                     </Card.Body>
                 </Card>
+                <div className="Login-links mt-3">
+                    <Link to="/register">You do not have an account?</Link>
+                </div>
             </div>
         </Container>
     );
