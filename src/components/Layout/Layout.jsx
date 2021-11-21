9a4510566c16857f212dccb3d8ad9928a15aa675
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2021-11-21 00:04:09 -0500

    Add toggle

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Header from '@layout/Header';
 import Sidebar from '@layout/Sidebar';
 
 const Layout = ({ children }) => {
+    const [toggle, setToogle] = useState(true);
+
+    const handleToogle = () => {
+        setToogle(!toggle);
+    }
+
     return (
         <Container fluid>
-            <div className="d-flex flex-direction-row align-items-start">
-                <Sidebar />
-                <div className="flex-fill">
-                    <Header />
-                    <div className="content mt-4">{children}</div>
+            <div className="row">
+                {toggle ?
+                    <div className="d-none d-md-block col-md-3 col-lg-2 p-0 m-0">
+                        <Sidebar />
+                    </div>
+                    : ''
+                }
+                <div className={ `${toggle ? 'col-md-9 col-lg-10' : 'col'} p-0 m-0` } >
+                    <Header handleToogle={handleToogle} />
+                    <div className="content m-4">{children}</div>
                 </div>
             </div>
         </Container>
